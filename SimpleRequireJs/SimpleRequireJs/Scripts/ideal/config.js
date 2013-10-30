076e4ebd09e9df7c7a785e31925a6252acfb68b1
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-10-30 09:31:19 -0500

    Setting up the demo code

--- SimpleRequireJs/SimpleRequireJs/Scripts/ideal/config.js
+++
@@ -1,3 +0,0 @@
-﻿define({
-   loggingEnabled: true
-});
--- SimpleRequireJs/SimpleRequireJs/Scripts/ideal/main.js
+++
@@ -1,9 +0,0 @@
-﻿define(['logger', 'messageGenerator'], function(logger, messageGenerator) {
-
-   $(function() {
-      logger.log("Adding message");
-      $('.message').text(messageGenerator.getMessage());
-      logger.log("Message added");
-   });
-   
-});
