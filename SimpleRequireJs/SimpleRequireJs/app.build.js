8c2a02ee726e2d2384e15fbb0ab3dd43bf9f71cf
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-10-29 22:21:26 -0500

    Bundling

---
+++ SimpleRequireJs/SimpleRequireJs/app.build.js
@@ -0,0 +1,22 @@
+{
+  "baseUrl": "C:\\Presentations\\RequireJs\\SimpleRequireJs\\SimpleRequireJs\\Scripts\\app",
+  "mainConfigFile": "C:\\Presentations\\RequireJs\\SimpleRequireJs\\SimpleRequireJs\\Scripts\\app\\main.js",
+  "include": [
+    "config",
+    "logger",
+    "main",
+    "messageGenerator",
+    "random"
+  ],
+  "exclude": [],
+  "keepBuildDir": true,
+  "optimize": "uglify2",
+  "out": "C:\\Presentations\\RequireJs\\SimpleRequireJs\\SimpleRequireJs\\appbuilt\\main.js",
+  "pragmas": {
+    "build": true
+  },
+  "wrap": true,
+  "insertRequire": [
+    "main"
+  ]
+}
---
+++ SimpleRequireJs/SimpleRequireJs/buildjs.bat
@@ -0,0 +1,2 @@
+rem optimize javascript
+node scripts\r.js -o scripts\app.build.js 
